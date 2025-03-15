6924ad60c674a9b681dc08c195c4322b79927523
Darshan Godase <darshangodase10@gmail.com>
2025-03-15 22:43:13 +0530

    Create a Admin Dashboard for assign Patients

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,14 +8,15 @@ import Signup from "./pages/Signup"
 import HomePage from './pages/HomePage';
 import Header from './components/Header';
 import Dashboard from "./pages/Dashboard";
-// import Preloader from "./components/Preloader";
-// import { AuthProvider } from "./context/AuthContext";
-
+import Preloader from "./components/Preloader";
+import { AuthProvider } from "./context/AuthContext";
+import AdminPage from "./pages/AdminPage";
+import TalkAI from "./TalkAI";
 function App() {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    // Simulate initial loading or do actual data fetching
+  
     const timer = setTimeout(() => {
       setLoading(false);
     }, 2000);
@@ -25,21 +26,25 @@ function App() {
 
   return (
     <>
-      
+      <AnimatePresence mode='wait'>
+        {loading && <Preloader setLoading={setLoading} />}
+      </AnimatePresence>
       
       {!loading && (
-        // <AuthProvider>
+        <AuthProvider>
           <Router>
             <ToastContainer position="top-right" autoClose={3000} />
             <Header />
             <Routes>
               <Route path="/" element={<HomePage />} />
+              <Route path="/admin" element={<AdminPage />} />
               <Route path="/dashboard" element={<Dashboard />} />
               <Route path="/login" element={<LoginForm />} />
               <Route path="/register" element={<Signup />} />
+              <Route path="/talk" element={<TalkAI />} />
             </Routes>
           </Router>
-        // </AuthProvider>
+        </AuthProvider>
       )}
     </>
   );
